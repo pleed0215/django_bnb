b1adb3d677c9e7b9d6d0f4b84d4bf54d581edecd
pleed0215 <pleed0215@icloud.com>
2020-08-30 13:47:57 +0900

    C:/Program Files/Git/

--- config/settings.py
+++ config/settings.py
@@ -180,7 +180,7 @@ if not DEBUG:
     AWS_BUCKET_ACL = "public-read"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"    
-    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static"    
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"    
     
 
 # Modifying user model
